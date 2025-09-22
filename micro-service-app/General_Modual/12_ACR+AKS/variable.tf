4954fb745cf93324a1f665bb3d15a42dab7d667e
surya-devops1984 <suryajio.agra@gmail.com>
2025-09-22 21:26:10 +0530

    Create variable.tf

---
+++ micro-service-app/General_Modual/12_ACR+AKS/variable.tf
@@ -0,0 +1,8 @@
+variable "acr_name" {}
+variable "acr_sku" {}
+variable "rg_name" {}
+variable "aks_name" {}
+variable "aks_dns_prefix" {}
+variable "aks_node_pool_name" {}
+variable "aks_node_count" {}
+variable "aks_node_vmsize" {}
