2062d317361444046f4948cfae05b42f4cc5d732
surya-devops1984 <suryajio.agra@gmail.com>
2025-09-22 22:24:31 +0530

    Update main.tf

--- micro-service-app/Env/dev/main.tf
+++ micro-service-app/Env/dev/main.tf
@@ -1,6 +1,6 @@
 module "rg" {
   source = "../../General_Modual/1Resource Group"
-  for_each = var.rg
-  rg_name = each.value.name
-  rg_location = each.value.location
+  rg_name = "suryarg"
+  rg_location = "central india"
 }
+
